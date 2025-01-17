5a22f94e8a9f86c474d30102c53b753f0e8f49f7
mesepith <zahiralam29@gmail.com>
2025-01-17 19:23:58 +0530

    testing

--- VoiceNoteApp/App.tsx
+++ VoiceNoteApp/App.tsx
@@ -42,7 +42,7 @@ const App = () => {
       });
 
       const data = await response.json();
-      console.log('data.transcription 1: ');
+      console.log('data.transcription: ', data.transcription);
       setTranscription(data.transcription);
     } catch (error) {
       console.error("Error stopping recording:", error);
